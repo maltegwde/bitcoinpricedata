dfaf4a994cef06a940505014cd87ff3d03b839be
Malte Schneider <maltegwde@gmail.com>
2020-08-23 14:07:53 +0200

    merged fixjson and requestdata

--- request_data.py
+++ request_data.py
@@ -10,9 +10,13 @@ from config import getCryptoId
 # 20000*60 = 1.200.000 seconds per request
 # 1577836800
 
-starttime = convert_to_unix(datetime(2014, 6, 1))
+starttime = convert_to_unix(datetime(2014, 1, 1))
 endtime = convert_to_unix(datetime(2020, 8, 19) + timedelta(minutes=5))
 limit = 4000
+
+crypto_id = getCryptoId()
+
+filename = "json/" + crypto_id + ".json"
 
 i = 0
 c = limit*5*60  #constant which is added to utime after each run
@@ -20,9 +24,7 @@ c = limit*5*60  #constant which is added to utime after each run
 utime=starttime
 output = {}
 
-crypto_id = getCryptoId()
 url = 'https://api.coinpaprika.com/v1/tickers/' +  crypto_id + '/historical?start=' + str(utime) + '&limit=' + str(limit)
-
 
 while utime <= endtime:
     response = requests.get(url)
@@ -43,7 +45,59 @@ while utime <= endtime:
     i+=limit
     print(int(i/limit))
 
-print(output)
+obj = output
+kl = list(obj.keys()) #keylist
 
-with open('json/' + crypto_id + '.json', 'w') as outfile:
-    json.dump(output, outfile, indent=4)
+#with open('json/' + crypto_id + '.json', 'w') as outfile:
+#    json.dump(output, outfile, indent=4)
+
+missing_data = 0
+
+print(len(str(obj[kl[0]][0]['price'])))
+decimals = len(str(obj[kl[0]][0]['price']).split('.')[1])
+
+
+for i in range(len(kl)-1):
+  tmp = int(kl[i]) + 300
+  diff = int(kl[i+1]) - int(kl[i])
+
+  if not diff == 300:   #if there is a gap in the data
+    print("missing data at " + kl[i] + " | " + kl[i+1])
+    missing_data += 1
+    d2 = int(diff / 300)
+    d2 -= 1
+
+    pdiff = obj[kl[i+1]][0]['price'] - obj[kl[i]][0]['price']
+    pdiff = round(pdiff)
+
+    pdiff_per_step = pdiff/d2
+
+    utime = int(kl[i])
+    price = obj[kl[i]][0]['price']
+
+    for i in range(d2):
+      utime+=300
+      price += pdiff_per_step
+      price = round(price, int(decimals))
+
+      output[str(utime)] = []
+      output[str(utime)].append({
+        'price': price
+      })
+
+print("saving to file...")
+
+merged = {**obj, **output}
+
+percentage_of_missing_datapoints = 0
+
+if missing_data > 0:
+  percentage_of_missing_datapoints = (missing_data/len(kl)) * 100
+  
+with open(filename, 'w') as outfile:
+    json.dump(merged, outfile, indent=4, sort_keys=True)
+
+print()
+print(decimals)
+print("Fixed the price-file! %s datapoints were fixed. That's %0.2f%% of all %s datapoints" %(missing_data, percentage_of_missing_datapoints, len(kl)))
+
